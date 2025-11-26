62fd3a67b6d86779373a5aed731099dd80842d33
Panchal2305 <panchalpalak543@gmail.com>
2025-11-26 22:03:28 +0530

    Delete Date.java

--- Date.java
+++
@@ -1,9 +0,0 @@
-
-import java.time.LocalDate;
-
-public class Date {
-    public static void main(String[] args) {
-        LocalDate today = LocalDate.now();
-        System.out.println("Today's Date: " + today);
-    }
-}
