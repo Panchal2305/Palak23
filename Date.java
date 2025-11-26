f66d6ee57f9b4c2b4163dc0f369e60dac58dfc59
Panchal2305 <panchalpalak543@gmail.com>
2025-11-26 21:35:56 +0530

    Add files via upload

---
+++ Date.java
@@ -0,0 +1,9 @@
+
+import java.time.LocalDate;
+
+public class Date {
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+        System.out.println("Today's Date: " + today);
+    }
+}
