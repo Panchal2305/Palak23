f0e30b0792edfed40aa3418b47febc6346803241
Panchal2305 <panchalpalak543@gmail.com>
2025-11-26 22:04:54 +0530

    Delete Modifyhelloworld.java

--- Modifyhelloworld.java
+++
@@ -1,7 +0,0 @@
-public class Modifyhelloworld {
-    public static void main(String[]args) {
-        System.out.println("hello world");
-        System.out.println("java language");
-        System.out.println("java is a programming language");
-    }   
-}
